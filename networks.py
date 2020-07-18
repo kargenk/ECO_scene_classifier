aecddcad4e35c936c31bc40633fbd2f20502a9c1
gengen <gengen0630@gmail.com>
2020-07-19 00:12:41 +0900

    [add] ECO 2D Net Inception A module

--- networks.py
+++ networks.py
@@ -42,6 +42,36 @@ class BasicConv(nn.Module):
         out = self.conv3(c2)  # [64, 56, 56] -> [192, 28, 28]
         return out
 
+class Inception_A(nn.Module):
+    """ ECOの2D Netモジュール内のInceptionモジュールの1つ目 """
+    
+    def __init__(self):
+        super(Inception_A, self).__init__()
+
+        self.inception1 = Conv2D(192, 64, kernel_size=1, stride=1)
+        self.inception2 = nn.Sequential(
+            Conv2D(192, 64, kernel_size=1, stride=1),
+            Conv2D(64, 64, kernel_size=3, stride=1, padding=1),
+        )
+        self.inception3 = nn.Sequential(
+            Conv2D(192, 64, kernel_size=1, stride=1),
+            Conv2D(64, 96, kernel_size=3, stride=1, padding=1),
+            Conv2D(96, 96, kernel_size=3, stride=1, padding=1),
+        )
+        self.inception4 = nn.Sequential(
+            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
+            Conv2D(192, 32, kernel_size=1, stride=1),
+        )
+
+    def forward(self, x):
+        out1 = self.inception1(x)  # [192, 28, 28] -> [64, 28, 28]
+        out2 = self.inception2(x)  # [192, 28, 28] -> [64, 28, 28]
+        out3 = self.inception3(x)  # [192, 28, 28] -> [96, 28, 28]
+        out4 = self.inception4(x)  # [192, 28, 28] -> [32, 28, 28]
+        # channels方向に結合，shape: [64+64+96+32, 28, 28]
+        out = torch.cat([out1, out2, out3, out4], dim=1)
+        return out
+
 if __name__ == '__main__':
     input_tensor = torch.randn(1, 3, 224, 224)  # 入力用のテストtensor
 
@@ -49,3 +79,8 @@ if __name__ == '__main__':
     basic_conv = BasicConv()
     basic_out = basic_conv(input_tensor)
     print('Basic Conv output:', basic_out.shape)
+
+    # InceptionAモジュールのテスト
+    inception_a = Inception_A()
+    inception_a_out = inception_a(basic_out)
+    print('Inception A output:', inception_a_out.shape)
