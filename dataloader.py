3c2f35ed89394fd3c21a7c6925e0f4864e4f0a41
gengen <gengen0630@gmail.com>
2020-07-20 11:24:22 +0900

    [add] Preprocessing for Video Class

--- dataloader.py
+++ dataloader.py
@@ -2,7 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-import torchvision
+import csv
+
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
 def make_datapath_list(root_path):
     """
@@ -39,6 +47,99 @@ def make_datapath_list(root_path):
             video_list.append(video_img_directory_path)
     
     return video_list
+
+class GroupResize():
+    """
+    画像群をまとめてリサイズするクラス．
+    画像の短い方の辺がresizeに変換される(アスペクト比はそのまま)．
+    """
+
+    def __init__(self, resize, interpolation=Image.BILINEAR):
+        self.rescaler = transforms.Resize(resize, interpolation)
+    
+    def __call__(self, img_group):
+        return [self.rescaler(img) for img in img_group]
+
+class GroupCenterCrop():
+    """
+    画像群をまとめてクリッピングするクラス．
+    (crop_size, crop_size)の画像を切り出す．
+    """
+
+    def __init__(self, crop_size):
+        self.center_crop = transforms.CenterCrop(crop_size)
+
+    def __call__(self, img_group):
+        return [self.center_crop(img) for img in img_group]
+
+class GroupToTensor():
+    """ 画像群をまとめてtorch.tensorに変換するクラス． """
+
+    def __init__(self):
+        self.to_tensor = transforms.ToTensor()
+    
+    def __call__(self, img_group):
+        # 学習済みデータの形式に合わせるため，[0, 255]で扱う
+        return [self.to_tensor(img) * 255 for img in img_group]
+
+class GroupImgNormalize():
+    """ 画像群をまとめて標準化するクラス． """
+
+    def __init__(self, mean, std):
+        self.normalize = transforms.Normalize(mean, std)
+
+    def __call__(self, img_group):
+        return [self.normalize(img) for img in img_group]
+
+class Stack():
+    """
+    画像群を1つのtensorにまとめるクラス．
+    
+    Inputs
+    ----------
+    img_group : list(torch.tensor)
+        torch.Size([3, 224, 224])を要素とするリスト
+    """
+
+    def __call__(self, img_group):
+        # 元の学習データがBGRのため，x.flip(dims=[0])で色チャネルをRGB -> BGRに変換
+        # unsqueeze(dim=0)でframes用の次元を追加して，frames次元で結合
+        ret = torch.cat([(x.flip(dims=[0])).unsqueeze(dim=0)
+                         for x in img_group], dim=0)
+
+class VideoTransform():
+    """
+    動画を画像にしたファイルの前処理クラス．学習と推論時で異なる動作をする．
+    動画を画像に分割しているため，分割された画像群をまとめて前処理する．
+    """
+
+    def __init__(self, resize, crop_size, mean, std):
+        self.data_transform = {
+            'train' : transforms.Compose([
+                # DataAugumentation()  # 今回は無し
+                GroupResize(int(resize)),          # 画像群をまとめてリサイズ
+                GroupCenterCrop(crop_size),        # 画像群をまとめて切り抜き
+                GroupToTensor(),                   # torch.tensorに
+                GroupImgNormalize(mean, std),  # データを標準化
+                Stack()                            # frames次元で結合
+            ]),
+            'val' : transforms.Compose([
+                GroupResize(int(resize)),          # 画像群をまとめてリサイズ
+                GroupCenterCrop(crop_size),        # 画像群をまとめて切り抜き
+                GroupToTensor(),                   # torch.tensorに
+                GroupImgNormalize(mean, std),  # データを標準化
+                Stack()                            # frames次元で結合
+            ])
+        }
+    
+    def __call__(self, img_group, phase):
+        """
+        Parameters
+        ----------
+        phase : 'train' or 'val'
+            前処理のモード指定フラグ
+        """
+        return self.data_transform[phase](img_group)
 
 if __name__ == '__main__':
     root_path = './data/kinetics_videos/'
